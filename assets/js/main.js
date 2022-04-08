63d7818cec83389f7d847ffd776c7a72856f3ca5
neapol2002 <73583307+neapol2002@users.noreply.github.com>
2022-04-08 18:27:20 +0400

    cooming auto

---
+++ assets/js/main.js
@@ -0,0 +1,79 @@
+(function($){
+	"use strict";
+	jQuery(document).on('ready', function () {
+        
+        // Subscribe form
+		$(".newsletter-form").validator().on("submit", function (event) {
+			if (event.isDefaultPrevented()) {
+			// handle the invalid form...
+				formErrorSub();
+				submitMSGSub(false, "Please enter your email address.");
+			} else {
+				// everything looks good!
+				event.preventDefault();
+			}
+		});
+		function callbackFunction (resp) {
+			if (resp.result === "success") {
+				formSuccessSub();
+			}
+			else {
+				formErrorSub();
+			}
+		}
+		function formSuccessSub(){
+			$(".newsletter-form")[0].reset();
+			submitMSGSub(true, "Thank you for subscribing!");
+			setTimeout(function() {
+				$("#validator-newsletter").addClass('hide');
+			}, 4000)
+		}
+		function formErrorSub(){
+			$(".newsletter-form").addClass("animated shake");
+			setTimeout(function() {
+				$(".newsletter-form").removeClass("animated shake");
+			}, 1000)
+		}
+		function submitMSGSub(valid, msg){
+			if(valid){
+				var msgClasses = "validation-success";
+			} else {
+				var msgClasses = "validation-danger";
+			}
+			$("#validator-newsletter").removeClass().addClass(msgClasses).text(msg);
+		}
+		// AJAX MailChimp
+		$(".newsletter-form").ajaxChimp({
+			url: "https://envytheme.us20.list-manage.com/subscribe/post?u=60e1ffe2e8a68ce1204cd39a5&amp;id=42d6d188d9", // Your url MailChimp
+			callback: callbackFunction
+		});
+
+		// Ripple JS
+        $('.ripple-effect, .ripple-playing').ripples({
+            resolution: 512,
+            dropRadius: 20,
+            perturbance: 0.04,
+        });
+
+    });
+
+    // WOW JS
+	$(window).on ('load', function (){
+        if ($(".wow").length) { 
+            var wow = new WOW({
+            boxClass:     'wow',      // Animated element css class (default is wow)
+            animateClass: 'animated', // Animation css class (default is animated)
+            offset:       20,         // Distance to the element when triggering the animation (default is 0)
+            mobile:       true,       // Trigger animations on mobile devices (default is true)
+            live:         true,       // Act on asynchronously loaded content (default is true)
+          });
+          wow.init();
+        }
+	});
+
+	// Preloader JS
+	$(window).on('load', function() {
+		$('.preloader').fadeOut();
+	});
+    
+}(jQuery));
